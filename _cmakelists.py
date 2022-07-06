533354d3f4ff72875d354fe5422326e4ae6bdc08
Jules Fouchy <jules.fouchy@ntymail.com>
2022-07-06 13:51:46 +0200

    🔨 [setup.py] Generate tests' CMakeLists.txt

--- _cmakelists.py
+++ _cmakelists.py
@@ -1,7 +1,38 @@
 def setup_cmakelists(lib_name, cpp_version, is_header_only):
     from _utils import make_file
+    from os.path import join
+
     make_file('CMakeLists.txt', "cmake_minimum_required(VERSION 3.8)\n" +
               cmakelists_body(lib_name, cpp_version, is_header_only))
+
+    make_file(join('tests', 'CMakeLists.txt'), f"""cmake_minimum_required(VERSION 3.8)
+project({lib_name}-tests)
+
+add_executable(${{PROJECT_NAME}} tests.cpp)
+target_compile_features(${{PROJECT_NAME}} PRIVATE cxx_std_20)
+
+if (MSVC)
+    target_compile_options(${{PROJECT_NAME}} PRIVATE /WX /W4)
+else()
+    target_compile_options(${{PROJECT_NAME}} PRIVATE -Werror -Wall -Wextra -Wpedantic -pedantic-errors -Wconversion -Wsign-conversion)
+endif()
+
+set({lib_name.upper()}_ENABLE_WARNINGS_AS_ERRORS ON)
+add_subdirectory(.. ${{CMAKE_CURRENT_SOURCE_DIR}}/build/{lib_name})
+target_link_libraries(${{PROJECT_NAME}} PRIVATE {lib_name}::{lib_name})
+
+add_subdirectory(doctest)
+target_link_libraries(${{PROJECT_NAME}} PRIVATE doctest::doctest)
+""")
+
+
+def enable_warnings(lib_name):
+    return f"""if(MSVC)
+    target_compile_options({lib_name} PRIVATE /WX /W4)
+else()
+    target_compile_options({lib_name} PRIVATE -Werror -Wall -Wextra -Wpedantic -pedantic-errors -Wconversion -Wsign-conversion)
+endif()
+"""
 
 
 def cmakelists_body(lib_name, cpp_version, is_header_only):
@@ -26,11 +57,7 @@ target_sources({lib_name} PRIVATE
 
 if ({lib_name.upper()}_ENABLE_WARNINGS_AS_ERRORS)
     message("-- [{lib_name}] Enabling warnings as errors for {lib_name}")
-    if(MSVC)
-        target_compile_options({lib_name} PRIVATE /WX /W4)
-    else()
-        target_compile_options({lib_name} PRIVATE -Werror -Wall -Wextra -Wpedantic -pedantic-errors -Wconversion -Wsign-conversion)
-    endif()
+    {enable_warnings(lib_name)}
 else()
     message("-- [{lib_name}] Not using warnings as errors for {lib_name}")
 endif()
