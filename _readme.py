f928526d370c8cf4394eda3ab6e0094fe0a60499
Jules Fouchy <jules.fouchy@ntymail.com>
2022-07-07 00:21:55 +0200

    📝 [_readme.py] Explain how to include the library

--- _readme.py
+++ _readme.py
@@ -2,4 +2,16 @@ def setup_readme(lib_name):
     from _utils import make_file
     make_file('README.md', f"""# {lib_name}
 
+## Including
+
+To add this library to your project, simply add those two lines to your *CMakeLists.txt*:
+```cmake
+add_subdirectory(path/to/{lib_name})
+target_link_libraries(${{PROJECT_NAME}} PRIVATE {lib_name}::{lib_name})
+```
+
+Then include it as:
+```cpp
+#include <{lib_name}/{lib_name}.hpp>
+```
 """)
