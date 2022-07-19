112e2d8b17cb4b620bb46ea368cbca0c583d2caa
Jules Fouchy <jules.fouchy@ntymail.com>
2022-07-19 19:06:58 +0200

    🔨 [_readme.py] Explain how to run the tests

--- _readme.py
+++ _readme.py
@@ -14,4 +14,15 @@ Then include it as:
 ```cpp
 #include <{lib_name}/{lib_name}.hpp>
 ```
+
+## Running the tests
+
+Simply use "tests/CMakeLists.txt" to generate a project, then run it.
+
+<ins>*Detailed steps for VSCode:*</ins>
+
+- Go to *Settings* (<kbd>CTRL</kbd> + <kbd>,</kbd>), select *Workspace*, search for "Source Directory" and set it as `${{workspaceFolder}}/tests`.
+- Reload the window (<kbd>CTRL</kbd> + <kbd>SHIFT</kbd> + <kbd>P</kbd> and search for "Reload Window").
+- Delete the CMake cache (<kbd>CTRL</kbd> + <kbd>SHIFT</kbd> + <kbd>P</kbd> and search for "Delete cache and reconfigure").
+- Then the CMake extension should pick it up and you can run the tests as usual with the triangle icon.
 """)
