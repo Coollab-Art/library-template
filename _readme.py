3514e6a5a8d605fef23e9634f23399dff52f4b6e
Jules Fouchy <jules.fouchy@ntymail.com>
2023-03-24 18:46:41 +0100

    🤏 Add SYSTEM when including ImGui

--- _readme.py
+++ _readme.py
@@ -7,7 +7,7 @@ def setup_readme(lib_name, needs_imgui):
 To add this library to your project, simply add these {"three" if needs_imgui else "two"} lines to your *CMakeLists.txt*{" and replace `folder/containing/imgui` with the path to the parent folder containing *imgui*" if needs_imgui else ""}:
 ```cmake
 add_subdirectory(path/to/{lib_name}){f'''
-target_include_directories({lib_name} PRIVATE folder/containing/imgui)''' if needs_imgui else ""}
+target_include_directories({lib_name} SYSTEM PRIVATE folder/containing/imgui)''' if needs_imgui else ""}
 target_link_libraries(${{PROJECT_NAME}} PRIVATE {lib_name}::{lib_name})
 ```
 
